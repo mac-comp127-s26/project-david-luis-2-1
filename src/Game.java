c8ff901ac395537784787192430de7a9146b9759
Luis <lxochihua20@gmail.com>
2026-04-24 14:09:12 -0500

    create Game.java

---
+++ src/Game.java
@@ -0,0 +1,25 @@
+import java.util.ResourceBundle.Control;
+
+import edu.macalester.graphics.CanvasWindow;
+
+public class Game {
+     private CanvasWindow canvas;
+   
+     private Boolean runGame(){
+          return true;
+     }
+
+  
+}
+
+/*
+Methods:
+runGame
+ResetGame
+Movement
+Jump 
+Crouch
+
+
+*/
+// runGame
