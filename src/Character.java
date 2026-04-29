11dbed422f13bea8380c5551b3a5b7cc1deef7b1
Luis <lxochihua20@gmail.com>
2026-04-29 13:14:07 -0500

    Worked on the controls for the character

--- src/Character.java
+++ src/Character.java
@@ -1,15 +1,51 @@
 
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Rectangle;
-
+import edu.macalester.graphics.events.Key;
 public class Character {
    private Rectangle Rectangle;
    private CanvasWindow canvas;
+   private double x = 30;
+   private double y = 440;
+   private double speed = 5;
+   private double rateY = 0;
+   private boolean onGround = true;
+   private static final double JUMP_POWER = -10;
 
 
     public Character(CanvasWindow canvas) {
       this.canvas = canvas;
-      this.Rectangle = new Rectangle(30, 440, 40, 60);
+      this.Rectangle = new Rectangle(x, y, 40, 60);
+        
+      canvas.onKeyDown(key ->{
+        if (key.getKey() == Key.LEFT_ARROW){
+            x -= speed;
+        }
+        if (key.getKey()== Key.RIGHT_ARROW){
+            x += speed;
+        }
+        if (key.getKey() == Key.UP_ARROW && onGround){
+            rateY = JUMP_POWER;
+            onGround = false;
+        }
+        if (key.getKey() == Key.DOWN_ARROW){
+            y += speed;
+        }
+       Rectangle.setPosition(x, y);
+      });
+       canvas.animate(dt -> {
+            rateY += 0.5;
+            y += rateY;
+
+            if (y >= 440){
+                y = 440;
+                rateY = 0;
+                onGround = true;
+            }
+
+        });
+        Rectangle.setPosition(x, y);
+        
     }
 
    public void addToCanvas(CanvasWindow canvasWindow) {
@@ -20,6 +56,7 @@ public class Character {
         return Rectangle;
     } 
     
+
 } 
 
 
