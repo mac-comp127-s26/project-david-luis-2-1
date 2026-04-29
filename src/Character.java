dbe2d2d3bd65422265dffadd2cd3dcbf279dd891
Luis <lxochihua20@gmail.com>
2026-04-28 22:28:48 -0500

    current version of code

--- src/Character.java
+++ src/Character.java
@@ -1,53 +1,32 @@
-import java.security.Key;
+
+
 
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.Point;
 import edu.macalester.graphics.Rectangle;
 
+
 // use as reference: https://stackoverflow.com/questions/18384707/sprite-movement-in-java
 
+
 public class Character {
-   private GraphicsGroup character; 
-   private int speed = 10;
-   private Point directionX;
-   private Point directionY;
-   private static final Key UP_ARROW = null;
-   private static final Key DOWN_ARROW = null;
-   private static final Key RIGHT_ARROW = null;
-   private static final Key LEFT_ARROW = null;
-   private int GRAVITY = 10;
-   
+  public Rectangle placeHolder = new Rectangle(10, 10, 10, 10);
+  private CanvasWindow canvas;
+  private double charXCoordinate;
+  private double charYCoordinate;
 
-    private void moveRight(){
-           int speed = 5;
 
-     }
-     private void moveLeft(){
-
-     }
-     
-     private void jump(){
-        double 
-     }
-
-     private void crouch(){
-     
-     }
-
-   private void getKey(){
-      if (UP_ARROW != null){
-         jump();         
-      }
-      if (DOWN_ARROW != null){
-         crouch();
-      }
-      if (RIGHT_ARROW != null){
-         moveRight();
-      }
-      if (LEFT_ARROW != null){
-         moveLeft();
-      }
-   }
+  public void makeCharacter(){
+   canvas.add(placeHolder);
+  }
+  private double getXCoordinate(){
+     return charXCoordinate;
+  }
+  private double getYCoordinate(){
+     return charYCoordinate;
+  }
+ 
 }
+
 
