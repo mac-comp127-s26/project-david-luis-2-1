1c0b313705629251596a4f72262c9925118ebf60
Luis <lxochihua20@gmail.com>
2026-04-29 14:09:43 -0500

    Update Character.java

--- src/Character.java
+++ src/Character.java
@@ -2,15 +2,25 @@
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Rectangle;
 import edu.macalester.graphics.events.Key;
+
 public class Character {
    private Rectangle Rectangle;
    private CanvasWindow canvas;
    private double x = 30;
    private double y = 440;
    private double speed = 5;
+   private double rateX = 0;
    private double rateY = 0;
    private boolean onGround = true;
-   private static final double JUMP_POWER = -10;
+   private boolean leftMovement = false;
+   private boolean rightMovement = false;
+
+   private static final double JUMP_POWER = -400;
+   private static final double GRAVITY = 800;
+   private static final double FlOOR_Y = 439.5;
+   private static final double MOVE_SPEED = 180;
+   private static final double FRICTION = 0.95;
+
 
 
     public Character(CanvasWindow canvas) {
@@ -19,33 +29,50 @@ public class Character {
         
       canvas.onKeyDown(key ->{
         if (key.getKey() == Key.LEFT_ARROW){
-            x -= speed;
+            leftMovement = true;
         }
         if (key.getKey()== Key.RIGHT_ARROW){
-            x += speed;
+            rightMovement = true;
         }
         if (key.getKey() == Key.UP_ARROW && onGround){
             rateY = JUMP_POWER;
             onGround = false;
         }
-        if (key.getKey() == Key.DOWN_ARROW){
-            y += speed;
-        }
-       Rectangle.setPosition(x, y);
-      });
-       canvas.animate(dt -> {
-            rateY += 0.5;
-            y += rateY;
 
-            if (y >= 440){
-                y = 440;
+      });
+
+      canvas.onKeyUp(key -> {
+        if (key.getKey() == Key.LEFT_ARROW){
+            leftMovement = false;
+        } 
+        if(key.getKey() == Key.RIGHT_ARROW){
+            rightMovement = false;
+        }
+    });
+
+       canvas.animate(dt -> {
+        if (leftMovement){
+            rateX = -MOVE_SPEED;
+        } else if (rightMovement){
+            rateX = MOVE_SPEED;
+        } else {
+            rateX *= FRICTION;
+        }
+        
+
+
+            rateY += GRAVITY * dt;
+            y += rateY * dt;
+            x += rateX * dt;
+
+            if (y >= FlOOR_Y){
+                y = FlOOR_Y;
                 rateY = 0;
                 onGround = true;
             }
-
+            Rectangle.setPosition(x, y);
         });
-        Rectangle.setPosition(x, y);
-        
+      
     }
 
    public void addToCanvas(CanvasWindow canvasWindow) {
