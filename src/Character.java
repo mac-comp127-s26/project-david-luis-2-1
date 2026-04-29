08bc14ee22f4972ea09555da25417a4710523de3
dacuna67 <dacuna@macalester.edu>
2026-04-28 21:44:52 -0500

    Update Character.java

--- src/Character.java
+++ src/Character.java
@@ -1,25 +1,51 @@
+import java.security.Key;
+
 import edu.macalester.graphics.GraphicsGroup;
+import edu.macalester.graphics.Point;
 
 // use as reference: https://stackoverflow.com/questions/18384707/sprite-movement-in-java
 
 public class Character {
-    private GraphicsGroup character; 
+   private GraphicsGroup character; 
+   private int speed = 10;
+   private Point directionX;
+   private Point directionY;
+   private static final Key UP_ARROW = null;
+   private static final Key DOWN_ARROW = null;
+   private static final Key RIGHT_ARROW = null;
+   private static final Key LEFT_ARROW = null;
+   private int GRAVITY = 10;
    
 
+    private void moveRight(){
+           int speed = 5;
 
-
-    private void movement(){
-        int speed = 5;
+     }
+     private void moveLeft(){
 
      }
      
      private void jump(){
-        double jump 
+        double 
      }
 
      private void crouch(){
      
      }
 
+   private void getKey(){
+      if (UP_ARROW != null){
+         jump();         
+      }
+      if (DOWN_ARROW != null){
+         crouch();
+      }
+      if (RIGHT_ARROW != null){
+         moveRight();
+      }
+      if (LEFT_ARROW != null){
+         moveLeft();
+      }
+   }
 }
 
